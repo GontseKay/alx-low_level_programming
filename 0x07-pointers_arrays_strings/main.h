a6c8d7f927c122d3c7b73f5f4888829d27246462
GontseKay <gontsekay1@gmail.com>
2023-03-06 05:55:34 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char *_memset(char *5, char b, unsigned int n);
+char *_memset(void *5, char b, unsigned int n);
 int _putchar(char c);
 char *memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
